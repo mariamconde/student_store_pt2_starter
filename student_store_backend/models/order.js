8a237e9c74dcb03cf3085b8f2d2e13320b6e9469
Mariam Conde <Condemriam@gmail.com>
2022-07-05 17:46:44 -0400

    minor changes

--- student_store_backend/models/order.js
+++ student_store_backend/models/order.js
@@ -1,78 +1,51 @@
 const { BadRequestError } = require("../utils/errors")
 const db = require("../db")
-
 class Order {
+
   static async listOrdersForUser(user) {
-    const query = `
-      SELECT orders.id AS "orderId",
-             orders.customer_id AS "customerId",
-             od.quantity AS "quantity",
-             products.name AS "name",
-             products.price AS "price"
-      FROM orders
-        JOIN order_details AS od ON od.order_id = orders.id
-        JOIN products ON products.id = od.product_id
-      WHERE orders.customer_id = (SELECT id FROM users WHERE email = $1)
-    `
-    const result = await db.query(query, [user.email])
+      const results = await db.query(`
+          SELECT orders.id AS "orderId",
+                 orders.customer_id AS "customerId",
+                 order_details.quantity AS "quantity",
+                 products.name AS "name",
+                 products.price AS "price"
+          FROM orders
+              JOIN order_details ON orders.id = order_details.order_id
+              JOIN products ON products.id = order_details.product_id
+          WHERE orders.customer_id =
+              (SELECT id FROM users WHERE email = $1) 
+      `, [user.email])
+      return results.rows
 
-    return result.rows
+
   }
 
-  static async createOrder({ order, user }) {
-    if (!order || !Object.keys(order).length) {
-      throw new BadRequestError("No order info provided")
-    }
-    if (!user) {
-      throw new BadRequestError("No user provided")
-    }
+  static async createOrder({order, user}) {
+      const results = await db.query(`
+          INSERT INTO orders (customer_id)
+          VALUES ((SELECT id FROM users WHERE email = $1))
+          RETURNING id
+      `, [user.email])
 
-    // create a new order
-    const orderResult = await db.query(
-      `
-      INSERT INTO orders (customer_id) 
-      VALUES ((SELECT id FROM users WHERE email = $1))
-      RETURNING id
-    `,
-      [user.email]
-    )
-    // get orderId
-    const orderId = orderResult.rows[0].id
+      const orderId = results.rows[0]
 
-    // add the products to the order details table
-    Object.keys(order).forEach(async (productId) => {
-      const quantity = order[productId]
 
-      await db.query(
-        `
-        INSERT INTO order_details (order_id, product_id, quantity)
-        VALUES ($1, $2, $3)
-      `,
-        [orderId, productId, quantity]
-      )
-    })
 
-    return await Order.fetchOrderById(orderId)
+      // looping through each item in user's order and adding a row to order details for each product in order
+      order.forEach((product) => {
+          db.query(`
+              INSERT INTO order_details (
+                  order_id,
+                  product_id,
+                  quantity
+              )
+              VALUES ($1, $2, $3)
+          `, [orderId.id, product.id, product.quantity])
+      })
+      
   }
 
-  static async fetchOrderById(orderId) {
-    const result = await db.query(
-      `
-      SELECT orders.id AS "orderId",
-             orders.customer_id AS "customerId",
-             od.quantity AS "quantity",
-             products.name AS "name",
-             products.price AS "price"
-      FROM orders
-        JOIN order_details AS od ON od.order_id = orders.id
-        JOIN products ON products.id = od.product_id
-      WHERE orders.id = $1
-    `,
-      [orderId]
-    )
-
-    return result.rows
-  }
 }
+
 
 module.exports = Order
